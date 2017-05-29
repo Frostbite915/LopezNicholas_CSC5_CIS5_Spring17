0ab35506ec7c7187d413317b788371de7b649d79
Nicholas Lopez <Nicholas Lopez>
2017-05-28 22:38:08 -0700

    Signed-off-by: Nicholas Lopez <Nicholas Lopez>

---
+++ Projects/Project2/Project2_V1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Nicholas Lopez
+ * Created on May 25, 2017, 1:30 PM
+ * Purpose: Version 1 of Project 2 (Battleship)
+ */
+
+//System Libraries Here
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+//Function Prototype
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    
+    //Exit
+    return 0;
+    }
