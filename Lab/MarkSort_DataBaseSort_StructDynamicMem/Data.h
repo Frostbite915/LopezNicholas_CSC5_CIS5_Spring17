679c95a22df9fe9d6e8e9f1d9e649e8ff4085bc0
rcc <rcc>
2017-05-30 11:59:48 -0700

    Signed-off-by: rcc <rcc>

---
+++ Lab/MarkSort_DataBaseSort_StructDynamicMem/Data.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Data.h
+ * Author: Nicholas Lopez
+ * Created on May 30th, 2017, 10:20 AM
+ * Purpose:  Mark Sort implemented with a database approach and a structure
+ *           with Dynamic Memory
+ */
+
+#ifndef DATA_H
+#define DATA_H
+
+struct Data{
+    int utilize;
+    int *array;
+    int *index;
+};
+
+
+#endif /* DATA_H */
+
---
+++ Lab/MarkSort_DataBaseSort_WithStructure/Data.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Data.h
+ * Author: Nicholas Lopez
+ * Created on May 30th, 2017, 10:20 AM
+ * Purpose:  Mark Sort implemented with a database approach and a structure
+ */
+
+#ifndef DATA_H
+#define DATA_H
+
+const int SIZE = 200;
+
+struct Data{
+    int utilize;
+    int array[SIZE];
+    int index[SIZE];
+};
+
+
+#endif /* DATA_H */
+
